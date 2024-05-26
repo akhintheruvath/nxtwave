959003cbe2cbcc3e2a5ce109ad9175f694786395
Akhin <akhintheruvath2827@gmail.com>
2024-05-26 19:48:27 +0530

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: [],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '0',
+      },
+    },
     extend: {},
   },
   plugins: [],
