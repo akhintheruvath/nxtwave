c830fabf4ccb283ce45d40d9ecc9bebfd3c2d6b3
Akhin <akhintheruvath2827@gmail.com>
2024-06-18 19:12:58 +0530

    changing the image url

--- src/Components/ProfileImage.jsx
+++ src/Components/ProfileImage.jsx
@@ -2,7 +2,7 @@ export const ProfileImage = () => {
    return (
       <div className="relative w-10 h-10 overflow-hidden rounded-full">
          <img
-            src="https://s3-alpha-sig.figma.com/img/cf4d/3d9d/645049d8ead249354c5100844f0ee7bb?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=INwB~ujhSZAfwEqKNaDHlWSGLcS6h9079YGW6WVXzQl9kikr1i2uqOP70LCx53QHcDfb-RSo81BxRgdH6ZNTOqIdY1G54B-A4Ss5A9yRUjnie-EMgqDvOX~bjkkP2rylZNz8Uevhm14EQiDt9g~8gjJgVKITNFJTHIvT~teVb-QA9oUjwPFcvJiVXe9iaH0qbyMSYXBr2EtlaG1zP2C1VYX~Oa1jC04LiEg8Qx6EU6xFcVr1z8HzitSM14U0~1UbrtTlU6Ia8qdKfNtbYx4hUWudyj7hXpLyxh~cjxQETnWAVNNVjfnrED0lcq4bQ9BR5nGOMlnrRS~athgSiGrvAA__"
+            src="https://s3-alpha-sig.figma.com/img/cf4d/3d9d/645049d8ead249354c5100844f0ee7bb?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mW7Ly~99dn381pU9R3IR0Y20-KuiombvmBryyNR7daToc2hoP74bXoSDTGiDQYYsPs5CAImxmBmZ0GqQlXr6z2ocHO7-oerNy5p4NRooOEsH6AZv~4bhhNs8GHCfvMjPSISUdBWH98vPX5JbHUIB~PRAGt2MBojXDeZkXzUiqSyYeWODWP-eyCsPk9z9fRYPfqnpBrs7n0ZjKJOYR9Bbkr5Tgz9t6xLoDcOi-VJLYi4taLFyAqaDUhh8Fu0A3Zk5yYIRzruVMt~tMinDhWz9UHjRDyNv6Te1kJUsNaPDz66AV4Bi2ZP-0VXUnTHlBO1koUMSpfWL0fIedeuLWPQy0Q__"
             alt="Profile"
             className="absolute w-15 h-15 object-cover"
          />
