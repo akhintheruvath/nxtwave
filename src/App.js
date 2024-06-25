07872211fa9f2dbe61d81072abd73947ae3a78c2
Akhin <akhintheruvath2827@gmail.com>
2024-06-25 22:43:32 +0530

    Adding route and navigation to add resources page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Home } from "./Pages/Home";
+import { AddResource } from "./Pages/AddResource";
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-resource" element={<AddResource />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Components/HeaderWithButton.jsx
+++ src/Components/HeaderWithButton.jsx
@@ -1,13 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import { AddIcon } from "../Icons/AddIcon";
 import { NxtwaveLogo } from "../Icons/NxtwaveLogo";
 import { ProfileImage } from "./ProfileImage";
 
 export const HeaderWithButton = () => {
+   const navigate = useNavigate();
+   const handleAdd = () => {
+      navigate("/add-resource");
+   }
+
    return (
       <div className="flex justify-between items-center py-4 px-12 border-b border-[#D7DFE9] bg-white fixed top-0 left-0 w-full z-10">
          <NxtwaveLogo />
          <div className="flex">
-            <div className="flex bg-[#0B69FF] items-center px-5 py-2 rounded-md cursor-pointer">
+            <div
+               className="flex bg-[#0B69FF] items-center px-5 py-2 rounded-md cursor-pointer"
+               onClick={handleAdd}
+            >
                <AddIcon />
                <button className="ml-2 font-hkgrotesk font-semibold text-sm leading-6 text-white">ADD</button>
             </div>
